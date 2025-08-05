4f1ddb358323489494d36a9c9c9bee8570153a72
QOZU <64510789+QOZU@users.noreply.github.com>
2025-08-06 03:59:09 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/logentry/main.go
@@ -0,0 +1,23 @@
+// cmd/logentry/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"logentry/internal/logentry"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := logentry.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
